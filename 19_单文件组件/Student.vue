e971a1bff0ca67fe3cc82fc7b4a010ea0f642f90
hzc <hzc@hzc.com>
2023-01-13 22:50:54 +0800

    更新

---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生年龄：{{age}}</h2>
+  </div>
+</template>
+
+<script>
+  // 组件交互相关的代码（数据，方法等）
+  export default {
+    name: 'Student',
+    data() {
+      return {
+        name: 'Jack',
+        age: 18
+      }
+    },
+  }
+</script>
+
+<style>
+  /* 组件的样式 */
+</style>
